3cf60a1e8c6ac45374ab81b579d29334f1978436
Erno Jokela <38183216+Ejo0@users.noreply.github.com>
2022-03-06 12:56:10 +0200

    Week4 task5.4

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,7 +4,6 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.tavaroita = 0
         self._ostoslista = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -6,6 +6,7 @@ class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
         self.maito = Tuote("Maito", 3)
+        self.leipa = Tuote("Leipa", 5)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
@@ -18,3 +19,8 @@ class TestOstoskori(unittest.TestCase):
     def test_ostoskorin_hinta_yhden_tuotteen_jalkeen(self):
         self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_jalkeen_korin_lukumaara_kaksi(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.leipa)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
